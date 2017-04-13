89d2bd996f66cb4c1ffb794591f9c1c31c434e9e
Sean (Sangbeom) Yi <isuryun@gmail.com>
2017-04-12 23:33:09 -0400

    Add Camera movements

---
+++ Assets/Scripts/CameraDarkenController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraDarkenController : MonoBehaviour {
+
+    private Button myButton;
+
+    public float darken = 0f;
+
+    private bool bStart;
+    private bool bOnce;
+    
+    void Awake()
+    {
+        myButton = GetComponent<Button>(); // <-- you get access to the button component here
+
+        myButton.onClick.AddListener(() => { myFunctionForOnClickEvent(); });
+    }
+
+
+    void myFunctionForOnClickEvent()
+    {
+        bOnce = true;
+        bStart = true;
+       
+        darken = 1.0f;
+
+        GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(bOnce == true)
+        {
+            // set color of the panel - black
+            GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = new Color(0, 0, 0, 255);
+            bOnce = false;
+        }
+
+        if (darken > 0 && bStart == true)
+        {
+            darken -= 0.01f;            
+        }
+        else
+        {
+            if (bStart == true)
+            {
+                bStart = false;
+                GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/CameraShakeController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraShakeController : MonoBehaviour {
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shake = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.05f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    private Button myButton;
+    private bool bStart;
+    private bool bEnd;
+
+    void Awake()
+    {
+        myButton = GetComponent<Button>(); // <-- you get access to the button component here
+
+        myButton.onClick.AddListener(() => { myFunctionForOnClickEvent(); });
+    }
+    
+        
+    void myFunctionForOnClickEvent()
+    {
+        bStart = true;
+        shake = 1.0f;
+
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shake > 0 && bStart == true)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shake -= Time.deltaTime * decreaseFactor;
+            bEnd = true;
+        }
+        else
+        {
+            shake = 0f;
+            if(bEnd == true)
+            {
+                camTransform.localPosition = originalPos;
+                bEnd = false;
+                bStart = false;
+            }            
+        }
+    }
+}
+
+
+
